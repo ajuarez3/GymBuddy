7466e8f12a50ab30502c9d7517cf4f3b342c00d5
Angela Elisa Juarez <ajuarez3@uncc.edu>
2021-11-18 16:46:58 -0500

    Color file

--- app/src/main/java/com/example/gymbudd/MessageInboxActivity.java
+++ app/src/main/java/com/example/gymbudd/MessageInboxActivity.java
@@ -6,40 +6,45 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.gymbudd.ui.main.SectionsPagerAdapter;
 import com.example.gymbudd.databinding.ActivityMessageInboxBinding;
 
 public class MessageInboxActivity extends AppCompatActivity {
 
-    private ActivityMessageInboxBinding binding;
+    //private ActivityMessageInboxBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMessageInboxBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setContentView(R.layout.activity_message_inbox);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
+        ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
+        TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // binding = ActivityMessageInboxBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+
+        //SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        //ViewPager viewPager = binding.viewPager;
+        //viewPager.setAdapter(sectionsPagerAdapter);
+        //TabLayout tabs = binding.tabs;
+        //tabs.setupWithViewPager(viewPager);
+
     }
 }
